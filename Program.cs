40f9ed77492a8497d0db3adc0a2edeae774bc714
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-26 20:49:10 +0200

    Initial

--- Program.cs
+++ Program.cs
@@ -4,15 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bubble_CLI {
-    class Program {
-        static void Main(string[] args) {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+namespace Bubble {
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    class BCLIMain {
+        static void Main(string[] args) {
+            Console.WriteLine("The actual program comes later");
         }
     }
 }
